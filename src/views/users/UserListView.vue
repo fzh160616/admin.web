48680f7c3849c7f5ef8a326fc0813cf0e1693d48
HomeMacMini <homemacmini@HomeMacMinideMac-mini.local>
2026-02-23 22:12:02 +0900

    style: update last login format to YYYY-mm-dd HH:ii:ss

--- src/views/users/UserListView.vue
+++ src/views/users/UserListView.vue
@@ -137,7 +137,7 @@ function formatHKTime(value) {
 
   const s = new Intl.DateTimeFormat('zh-HK', {
     timeZone: 'Asia/Hong_Kong',
-    year: '2-digit',
+    year: 'numeric',
     month: '2-digit',
     day: '2-digit',
     hour: '2-digit',
